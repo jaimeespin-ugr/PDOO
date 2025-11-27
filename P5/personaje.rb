e314f8b5a0af0f7ef142ae3a9ca3ab5016afb7d4
jaime-espin <jaime.espinrodriguez@gmail.com>
2025-11-27 20:23:08 +0100

    Corrección ejercicio clase 5

--- P5/personaje.rb
+++ P5/personaje.rb
@@ -3,8 +3,8 @@ require_relative 'ser_vivo'
 class Personaje < Ser_vivo
   attr_accessor :tipo
 
-  @@TIPO_MONSTRUO = 'Monstruo'
-  @@TIPO_JUGADOR = 'Jugador'
+  TIPO_MONSTRUO = 'Monstruo'
+  TIPO_JUGADOR = 'Jugador'
 
   def initialize
     raise 'Error'
